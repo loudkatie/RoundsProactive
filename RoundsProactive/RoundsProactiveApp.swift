76c5568731662eb700b0e73013423ccf20cd88a5
Katie Richman <katie@loudlabs.xyz>
2025-11-02 06:54:06 -0500

    Initial Commit

---
+++ RoundsProactive/RoundsProactiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoundsProactiveApp.swift
+//  RoundsProactive
+//
+//  Created by Katie Richman on 11/2/25.
+//
+
+import SwiftUI
+
+@main
+struct RoundsProactiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
